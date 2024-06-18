a187bf0da33a6c942e39c866305f6af0351d71dd
sherncrln <sherncrln@gmail.com>
2024-06-19 01:11:05 +0700

    updating class.php

---
+++ class.php
@@ -0,0 +1,58 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: *");
+header("Access-Control-Allow-Methods: *");
+
+
+include 'connection.php';
+$objDb = new Connection;
+$conn = $objDb->connect();
+
+$method = $_SERVER['REQUEST_METHOD'];
+
+switch($method) {
+    case "GET":
+        $sql = "SELECT * FROM class";
+        $path = explode('/', $_SERVER['REQUEST_URI']);
+        if(isset($path[3]) && is_numeric($path[3])) {
+            $sql .= " WHERE class_id = :id";
+            $stmt = $conn->prepare($sql);
+            $stmt->bindParam(':id', $path[3]);
+            $stmt->execute();
+            $users = $stmt->fetch(PDO::FETCH_ASSOC);
+        } else {
+            $stmt = $conn->prepare($sql);
+            $stmt->execute();
+            $users = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        }
+        echo json_encode($users);
+        break;
+    case "PUT":
+        // $user = json_decode(file_get_contents('php://input'), true);
+        // if ($user && is_array($user)) {
+        //     //Update the user data
+        //     $sql = "UPDATE `user` SET `name` = :name, `class` = :class , `category` = :category, `status_user` = :status_user, `email` = :email, `updated_at` = :updated_at WHERE `user_id` = :id ;";
+        //     $stmt = $conn->prepare($sql);
+        //     $updated_at = date('Y-m-d');
+        //     $stmt->bindParam(':id', $user['user_id'], PDO::PARAM_INT);
+        //     $stmt->bindParam(':name', $user['name']);
+        //     $stmt->bindParam(':class', $user['class']);
+        //     $stmt->bindParam(':category', $user['category']);
+        //     $stmt->bindParam(':email', $user['email']);
+        //     $stmt->bindParam(':status_user', $user['status_user']);
+        //     $stmt->bindParam(':updated_at', $updated_at);
+        //     if($stmt->execute()){
+        //         $response = ['status' => 1, 'message' => 'Record updated successfully.'];
+        //     }else{
+        //         $response = ['status' => 0, 'message' => 'Failed to update record.'];            
+        //     }
+        //     echo json_encode($response);
+        // } else {
+        //     // $response = "hello this is else ";
+        //     $response = ["error" => "Invalid request data"];
+        //     echo json_encode($response);
+        // }
+        break;
+}
