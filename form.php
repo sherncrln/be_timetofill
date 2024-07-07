4d7ec945efd880d3bfd98ff8376facd156a916eb
sherncrln <sherncrln@gmail.com>
2024-07-07 20:20:34 +0700

    update form POST-Duplicate

--- form.php
+++ form.php
@@ -29,51 +29,101 @@ switch($method) {
         break;
 
     case "POST":
-        $form = json_decode(file_get_contents('php://input'), true);
-
-        if ($form && is_array($form)) {
-            $sql = "INSERT INTO `form` (`name_form`, `status_form`, `respondent`, `show_username`, `description`, `created_at`, `updated_at`) VALUES (:name_form, :status_form, :respondent, :show_username, :description, :created_at, :updated_at)";
+        if (isset($_GET['form_id']) && is_numeric($_GET['form_id'])) {
+            $form_id = $_GET['form_id'];
+            
+            $sql = "SELECT * FROM form WHERE form_id = :form_id";
             $stmt = $conn->prepare($sql);
-            $updated_at = date('Y-m-d H:i:s', strtotime('+7 hour'));
-            $created_at = date('Y-m-d H:i:s', strtotime('+7 hour'));
-            $stmt->bindParam(':name_form', $form['name_form']);
-            $stmt->bindParam(':status_form', $form['status_form']);
-            $stmt->bindParam(':respondent', $form['respondent']);
-            $stmt->bindParam(':show_username', $form['show_username']);
-            $stmt->bindParam(':description', $form['description']);
-            $stmt->bindParam(':created_at', $created_at);
-            $stmt->bindParam(':updated_at', $updated_at);
+            $stmt->bindParam(':form_id', $form_id, PDO::PARAM_INT);
+            $stmt->execute();
+            $form = $stmt->fetch(PDO::FETCH_ASSOC);
 
-            if ($stmt->execute()) {
-                $form_id = $conn->lastInsertId();
+            if ($form) {
+                $sql_insert = "INSERT INTO `form` (`name_form`, `status_form`, `respondent`, `show_username`, `description`, `created_at`, `updated_at`) VALUES (:name_form, :status_form, :respondent, :show_username, :description, :created_at, :updated_at)";
+                $stmt_insert = $conn->prepare($sql_insert);
+                $updated_at = date('Y-m-d H:i:s', strtotime('+7 hour'));
+                $created_at = date('Y-m-d H:i:s', strtotime('+7 hour'));
+                $stmt_insert->bindParam(':name_form', $form['name_form']);
+                $stmt_insert->bindParam(':status_form', $form['status_form']);
+                $stmt_insert->bindParam(':respondent', $form['respondent']);
+                $stmt_insert->bindParam(':show_username', $form['show_username']);
+                $stmt_insert->bindParam(':description', $form['description']);
+                $stmt_insert->bindParam(':created_at', $created_at);
+                $stmt_insert->bindParam(':updated_at', $updated_at);
 
-                if (isset($form['question']) && is_array($form['question'])) {
-                    $question = json_encode($form['question']);
-                    $qtype = json_encode($form['qtype']);
-                    $sql_detail = "INSERT INTO `form_detail` (`form_id`, `question`, `qtype`) VALUES (:form_id, :question, :qtype)";
+                if ($stmt_insert->execute()) {
+                    $new_form_id = $conn->lastInsertId();
+                    $sql_detail = "SELECT * FROM form_detail WHERE form_id = :form_id";
                     $stmt_detail = $conn->prepare($sql_detail);
                     $stmt_detail->bindParam(':form_id', $form_id, PDO::PARAM_INT);
-                    $stmt_detail->bindParam(':question', $question);
-                    $stmt_detail->bindParam(':qtype', $qtype);
+                    $stmt_detail->execute();
+                    $form_details = $stmt_detail->fetchAll(PDO::FETCH_ASSOC);
 
-                    if ($stmt_detail->execute()) {
-                        $response = ['status' => 1, 'message' => 'Record updated successfully.'];
+                    foreach ($form_details as $detail) {
+                        $sql_insert_detail = "INSERT INTO form_detail (form_id, question, qtype) VALUES (:form_id, :question, :qtype)";
+                        $stmt_insert_detail = $conn->prepare($sql_insert_detail);
+                        $stmt_insert_detail->bindParam(':form_id', $new_form_id, PDO::PARAM_INT);
+                        $stmt_insert_detail->bindParam(':question', $detail['question']);
+                        $stmt_insert_detail->bindParam(':qtype', $detail['qtype']);
+                        $stmt_insert_detail->execute();
+                    }
+
+                    $response = ['status' => 1, 'message' => 'Form duplicated successfully.'];
+                } else {
+                    $response = ['status' => 0, 'message' => 'Failed to duplicate form.'];
+                }
+            } else {
+                $response = ['status' => 0, 'message' => 'Form not found.'];
+            }
+
+            echo json_encode($response);
+        } else {
+            $form = json_decode(file_get_contents('php://input'), true);
+
+            if ($form && is_array($form)) {
+                $sql = "INSERT INTO `form` (`name_form`, `status_form`, `respondent`, `show_username`, `description`, `created_at`, `updated_at`) VALUES (:name_form, :status_form, :respondent, :show_username, :description, :created_at, :updated_at)";
+                $stmt = $conn->prepare($sql);
+                $updated_at = date('Y-m-d H:i:s', strtotime('+7 hour'));
+                $created_at = date('Y-m-d H:i:s', strtotime('+7 hour'));
+                $stmt->bindParam(':name_form', $form['name_form']);
+                $stmt->bindParam(':status_form', $form['status_form']);
+                $stmt->bindParam(':respondent', $form['respondent']);
+                $stmt->bindParam(':show_username', $form['show_username']);
+                $stmt->bindParam(':description', $form['description']);
+                $stmt->bindParam(':created_at', $created_at);
+                $stmt->bindParam(':updated_at', $updated_at);
+
+                if ($stmt->execute()) {
+                    $form_id = $conn->lastInsertId();
+
+                    if (isset($form['question']) && is_array($form['question'])) {
+                        $question = json_encode($form['question']);
+                        $qtype = json_encode($form['qtype']);
+                        $sql_detail = "INSERT INTO `form_detail` (`form_id`, `question`, `qtype`) VALUES (:form_id, :question, :qtype)";
+                        $stmt_detail = $conn->prepare($sql_detail);
+                        $stmt_detail->bindParam(':form_id', $form_id, PDO::PARAM_INT);
+                        $stmt_detail->bindParam(':question', $question);
+                        $stmt_detail->bindParam(':qtype', $qtype);
+
+                        if ($stmt_detail->execute()) {
+                            $response = ['status' => 1, 'message' => 'Record updated successfully.'];
+                        } else {
+                            $response = ['status' => 0, 'message' => 'Failed to update record.'];
+                        }
+                        echo json_encode($response);
                     } else {
                         $response = ['status' => 0, 'message' => 'Failed to update record.'];
+                        echo json_encode($response);
+                        exit;
                     }
-                    echo json_encode($response);
                 } else {
                     $response = ['status' => 0, 'message' => 'Failed to update record.'];
                     echo json_encode($response);
-                    exit;
                 }
             } else {
-                $response = ['status' => 0, 'message' => 'Failed to update record.'];
+                $response = ["error" => "Data is not valid"];
                 echo json_encode($response);
             }
-        } else {
-            $response = ["error" => "Data permintaan tidak valid"];
-            echo json_encode($response);
         }
         break;
         
