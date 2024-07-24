ac0f4bbae231ff01e4cdd918519ed7b5e4de3243
sherncrln <sherncrln@gmail.com>
2024-07-24 17:27:29 +0700

    Response Detail UI

--- response.php
+++ response.php
@@ -19,7 +19,7 @@ switch($method) {
             $timestamp = date('Y-m-d H:i:s', strtotime('+7 hour'));
             $answer = json_encode($res[3]);
             
-            $sql = "INSERT INTO `response` (`form_id`, `timestamp`, `class`, `username`, `answer`) VALUES (:form_id, :timestamp,  :class, :username, :answer)";
+            $sql = "INSERT INTO `response` (`form_id`, `timestamp`, `class`, `username`, `answer`) VALUES (:form_id, :timestamp,  :class, :username, :answer);";
             $stmt = $conn->prepare($sql);
             $stmt->bindParam(':form_id', $id, PDO::PARAM_INT);
             $stmt->bindParam(':class', $res[1]);
@@ -41,13 +41,13 @@ switch($method) {
 
     case "GET":
         if(isset($_GET['form_id']) && is_numeric($_GET['form_id'])) {
-            $sql = "SELECT f.form_id, f.name_form, f.status_form, f.show_username, f.respondent, fd.question FROM form f JOIN form_detail fd ON f.form_id = fd.form_id WHERE f.form_id = :id ;";
+            $sql = "SELECT f.form_id, f.name_form, f.status_form, f.show_username, f.respondent, fd.question, fd.qtype FROM form f JOIN form_detail fd ON f.form_id = fd.form_id WHERE f.form_id = :id ;";
             $stmt = $conn->prepare($sql);
             $stmt->bindParam(':id', $_GET['form_id'], PDO::PARAM_INT);
             $stmt->execute();
             $head = $stmt->fetch(PDO::FETCH_ASSOC);
             if ($stmt->execute()) {
-                $sql_detail = "SELECT r.response_id, r.timestamp, r.class, u.name AS name, r.answer, f.form_id FROM response r JOIN form_detail f ON r.form_id = f.form_id JOIN user u ON r.username = u.username WHERE f.form_id = :id ;";
+                $sql_detail = "SELECT r.response_id, r.timestamp, r.class, u.name, r.answer, f.form_id FROM response r JOIN form_detail f ON r.form_id = f.form_id JOIN user u ON r.username = u.username WHERE f.form_id = :id ;";
                 $stmt_detail = $conn->prepare($sql_detail);
                 $stmt_detail->bindParam(':id', $_GET['form_id'], PDO::PARAM_INT);
                 $stmt_detail->execute();
